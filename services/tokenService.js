d1a7364432cc8b3509f49afbfb262c73cf74ea08
daria-pro <shaulkod@gmail.com>
2022-12-12 17:32:07 +0200

    change token time

--- services/tokenService.js
+++ services/tokenService.js
@@ -6,7 +6,7 @@ dotenv.config()
 class TokenService {
   generateToken(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-      expiresIn: '30d',
+      expiresIn: '15m',
     })
 
     const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
--- utils/isOwner.js
+++
@@ -1,1 +0,0 @@
-const isOwner = async (id, userId) => {};

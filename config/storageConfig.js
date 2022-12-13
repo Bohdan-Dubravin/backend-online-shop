c067efdf33bd37a5ceccef5cd7e8794693a58961
Bohdan-Dubravin <dubravin111@gmail.com>
2022-12-13 21:59:59 +0200

    add cloud images

--- config/storageConfig.js
+++ config/storageConfig.js
@@ -1,30 +1,26 @@
-import multer from 'multer'
-import { CloudinaryStorage } from 'multer-storage-cloudinary'
-import { v2 as cloudinary } from 'cloudinary'
-import * as dotenv from 'dotenv'
-dotenv.config()
+// import multer from 'multer';
+// import { CloudinaryStorage } from 'multer-storage-cloudinary';
+// import { v2 as cloudinary } from 'cloudinary';
+// import * as dotenv from 'dotenv';
+// // dotenv.config()
 
-export default cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
-})
+// // export default
 
-// const storage = new CloudinaryStorage({
-//   cloudinary,
-//   params: {
-//     folder: 'CloudinaryDemo',
-//     allowedFormats: ['jpeg', 'png', 'jpg'],
-//   },
-// })
+// // const storage = new CloudinaryStorage({
+// //   cloudinary,
+// //   params: {
+// //     folder: 'CloudinaryDemo',
+// //     allowedFormats: ['jpeg', 'png', 'jpg'],
+// //   },
+// // })
 
-// const storage = multer.diskStorage({
-//   destination: (_, __, callback) => {
-//     callback(null, 'uploads')
-//   },
-//   filename: (_, file, callback) => {
-//     callback(null, file.originalname)
-//   },
-// })
+// // const storage = multer.diskStorage({
+// //   destination: (_, __, callback) => {
+// //     callback(null, 'uploads')
+// //   },
+// //   filename: (_, file, callback) => {
+// //     callback(null, file.originalname)
+// //   },
+// // })
 
-// export default storage
+// // export default storage
--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -1,21 +1,32 @@
-import multer from 'multer'
-import storage from '../config/storageConfig.js'
-import { Router } from 'express'
-import checkAuth from '../middleware/checkAuth.js'
-import cloudinary from '../config/storageConfig.js'
-const upload = multer({ storage })
+import multer from 'multer';
 
-const uploadRouter = new Router()
+import { Router } from 'express';
+import checkAuth from '../middleware/checkAuth.js';
+import { v2 as cloudinary } from 'cloudinary';
+// const upload = multer({ storage })
+
+const uploadRouter = new Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 uploadRouter.post('/', async (req, res, next) => {
   try {
-    console.log(req.body)
-    const result = await cloudinary.uploader.upload(req.body, options)
-    console.log(result)
-    res.status(200).json({ message: 'ok' })
+    const result = await cloudinary.uploader.upload(
+      req.files.image.tempFilePath,
+      {
+        public_id: `${Date.now()}`,
+        resource_type: 'auto',
+        folder: 'images',
+      }
+    );
+    res.status(200).json(result.url);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-export default uploadRouter
+export default uploadRouter;

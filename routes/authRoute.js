a59f49c5325153e84bd02ed714fa8525729dcaa9
Bohdan-Dubravin <dubravin111@gmail.com>
2022-12-17 12:04:42 +0200

    change route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,27 +1,27 @@
-import { Router } from 'express'
-import authController from '../controllers/authController.js'
-import checkAuth from '../middleware/checkAuth.js'
-import checkRole from '../middleware/checkRole.js'
-import loginLimiter from '../middleware/loginLimit.js'
-import { registerValidation } from '../validations/Validations.js'
+import { Router } from 'express';
+import authController from '../controllers/authController.js';
+import checkAuth from '../middleware/checkAuth.js';
+import checkRole from '../middleware/checkRole.js';
+import loginLimiter from '../middleware/loginLimit.js';
+import { registerValidation } from '../validations/Validations.js';
 
-const authRouter = new Router()
+const authRouter = new Router();
 
 authRouter.post(
   '/register',
   loginLimiter,
   registerValidation,
   authController.register
-)
-authRouter.post('/login', loginLimiter, authController.login)
-authRouter.post('/logout', authController.logout)
-authRouter.post('/refresh', authController.refresh)
-authRouter.get('/users', checkAuth, checkRole, authController.getUsers)
+);
+authRouter.post('/login', loginLimiter, authController.login);
+authRouter.post('/logout', authController.logout);
+authRouter.post('/refresh', authController.refresh);
+authRouter.post('/users', checkAuth, checkRole, authController.getUsers);
 authRouter.delete(
   '/delete/:id',
   checkAuth,
   checkRole,
   authController.deleteUser
-)
+);
 
-export default authRouter
+export default authRouter;
